289da886d9c5dd141adfc507b5a0108b356b5fa3
Kim Seong Min <bomulsm@tukorea.ac.kr>
2025-11-04 21:12:51 +0900

    새 날갯짓 속도 및 비행 속도 조절하기 위한 코드 추가

--- bird.py
+++ bird.py
@@ -18,6 +18,12 @@ class Bird:
         self.total_frames = 13
         self.frame_per_row = 5
 
+        self.frame_time = 0
+        self.frame_interval = 0.05
+
+        self.screen_width = get_canvas_width()
+        self.screen_height = get_canvas_height()
+
     def draw(self):
         col = self.frame % self.frame_per_row # 최대 5프레임
         row = self.frame // self.frame_per_row # 최대 3행
@@ -27,7 +33,11 @@ class Bird:
             self.image.clip_draw(col * self.frame_width, row * self.frame_height, self.frame_width, self.frame_height, self.x, self.y, 100, 100)
         else:
             self.image.clip_draw(col * self.frame_width, row * self.frame_height, self.frame_width, self.frame_height, self.x, self.y, 100, 100)
-   
+
     def update(self):
-        self.frame = (self.frame + 1) % self.total_frames
-        delay(0.1)
+        self.x += self.speed * self.direction * game_framework.frame_time
+        self.frame_time += game_framework.frame_time
+
+        if self.frame_time >= self.frame_interval:
+          self.frame = (self.frame + 1) % self.total_frames
+          self.frame_time = 0

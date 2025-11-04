1ad4a2b6b01305aed0415fbb87b81419bec74f67
Kim Seong Min <bomulsm@tukorea.ac.kr>
2025-11-04 21:59:26 +0900

    캔버스 공간 픽셀/미터로 변환 및 새 크기 설정, 비행 속도 설정

--- bird.py
+++ bird.py
@@ -1,15 +1,22 @@
 from pico2d import *
 import game_framework
 
+PIXEL_PER_METER = (100.0 / 0.3)   # 약 333.33 (1미터 = 333.33픽셀)
+METER_PER_PIXEL = (0.3 / 100.0)   # 0.003 (1픽셀 = 0.3cm)
+
+
+BIRD_SPEED_MPS = 2.0  # 새가 초당 2미터 이동
+
 class Bird:
     image = None
 
-    def __init__(self, x=400, y=300, speed=200, direction=1, frame=0):
+    def __init__(self, x=400, y=300, direction=1, frame=0):
         if Bird.image == None:
             Bird.image = load_image('bird_animation.png')
         self.x = x
         self.y = y
-        self.speed = speed
+
+        self.speed = BIRD_SPEED_MPS * PIXEL_PER_METER
         self.direction = direction
         self.frame = frame
 
@@ -31,15 +38,23 @@ class Bird:
         if self.direction == 1:
             if row == 0:
                 col = self.frame % 4
-                self.image.clip_draw(col * self.frame_width, row * self.frame_height, self.frame_width, self.frame_height, self.x, self.y, 100, 100)
+                self.image.clip_draw(col * self.frame_width, row * self.frame_height,
+                                   self.frame_width, self.frame_height,
+                                   self.x, self.y, 100, 100)
             else:
-                self.image.clip_draw(col * self.frame_width, row * self.frame_height, self.frame_width, self.frame_height, self.x, self.y, 100, 100)
+                self.image.clip_draw(col * self.frame_width, row * self.frame_height,
+                                   self.frame_width, self.frame_height,
+                                   self.x, self.y, 100, 100)
         else:
             if row == 0:
                 col = self.frame % 4
-                self.image.clip_composite_draw(col * self.frame_width, row * self.frame_height, self.frame_width, self.frame_height, 0, 'h', self.x, self.y, 100, 100)
+                self.image.clip_composite_draw(col * self.frame_width, row * self.frame_height,
+                                              self.frame_width, self.frame_height,
+                                              0, 'h', self.x, self.y, 100, 100)
             else:
-                self.image.clip_composite_draw(col * self.frame_width, row * self.frame_height, self.frame_width, self.frame_height, 0, 'h', self.x, self.y, 100, 100)
+                self.image.clip_composite_draw(col * self.frame_width, row * self.frame_height,
+                                              self.frame_width, self.frame_height,
+                                              0, 'h', self.x, self.y, 100, 100)
 
     def update(self):
         self.x += self.speed * self.direction * game_framework.frame_time
